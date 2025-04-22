5556a680a4b1a853db11e4f6ca275799208269e0
root <rws@nextrade.co.kr>
2025-04-22 10:50:13 +0900

    add:main.py uvicorn

--- batch.sh
+++ batch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
+eval "$("$PYENV_ROOT/bin/pyenv" init --path)"
+eval "$("$PYENV_ROOT/bin/pyenv" init -)"
+eval "$("$PYENV_ROOT/bin/pyenv" virtualenv-init -)"
 
 echo "start:batch.sh"
 
 pyenv activate venv
+
 python3 /home/sysmon/network_admin/batch.py >> /home/sysmon/network_admin/batch.log 2>&1
 
 echo "end:batch.sh"
