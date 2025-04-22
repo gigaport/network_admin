a9eee54f7f808ced5a608ccd2dd623b87f01a939
root <rws@nextrade.co.kr>
2025-04-22 10:40:16 +0900

    add:main.py uvicorn

--- batch.sh
+++ batch.sh
@@ -7,6 +7,6 @@ eval "$(pyenv init -)"
 echo "start:batch.sh"
 
 pyenv active venv
-python3 /home/sysmon/network_admin/batch.py >> batch.log 2>&1
+python3 /home/sysmon/network_admin/batch.py >> /home/sysmon/network_admin/batch.log 2>&1
 
 echo "end:batch.sh"
