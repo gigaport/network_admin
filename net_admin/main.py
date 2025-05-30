f957ba52fb03ac3a3f5ace47c09464cf18475a46
root <rws@nextrade.co.kr>
2025-05-30 10:20:31 +0900

    edit:max traffic slack

--- net_admin/main.py
+++ net_admin/main.py
@@ -235,12 +235,15 @@ def send_message_to_slack(channel:str, message_info: Dict):
             attachments=[
                 {
                     "color": "warning",
-                    "title": f"{message_info['device']} // LEVEL:{message_info['severity']}",
+                    # "title": f"{message_info['device']} // LEVEL:{message_info['severity']}",
                     "text": (
                         f"*-장비이름: {message_info['device']}*\n"
                         f"-장비IP: `{message_info['host_ip']}`\n"
                         f"-발생일시: `{message_info['timestamp_trans']}`\n"
                         f"-LEVEL: `{message_info['severity'].upper()}`\n"
+                        f"-Facility: {message_info['facility']}\n"
+                        f"-Mnemonic: {message_info['mnemonic']}\n"
+                        f"-Type: {message_info['type']}\n"
                         f"-MESSAGE: ```{message_info['message']}```\n"
                     ),
                     "mrkdwn_in": ["text", "title"]
