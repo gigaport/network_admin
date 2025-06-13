1a010243f5f4f1642bbfc921068cbbee07c491e6
root <rws@nextrade.co.kr>
2025-06-13 13:26:09 +0900

    edit:information_site_created

--- net_admin/app/multicast/views.py
+++ net_admin/app/multicast/views.py
@@ -61,10 +61,10 @@ def init (request):
         client_info:Dict = openJsonFile(path)
         
         if sub_menu == "pr_multicast" or sub_menu == "pr_info_multicast": 
-            path = f"ps_mpr_multicast_info.json"
+            path = f"pr_mpr_multicast_info.json"
         elif sub_menu == "ts_multicast": 
             path = f"ts_mpr_multicast_info.json"
-            
+
         mpr_multicast_info:Dict = openJsonFile(path)
 
         ## 데이터 유무 검증
