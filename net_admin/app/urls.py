01e420be2ce9ba668fe98eca823a548c18f9019b
nextrws <rws@nextrade.co.kr>
2025-07-09 14:38:13 +0900

    app main 미사용 코드 삭제 테스트

--- net_admin/app/urls.py
+++ net_admin/app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     # path('multicast', views.multicast, name='multicast'),
 ]
