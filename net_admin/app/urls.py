4c6fd5d7792832e6c17023d564ec0c97495a031b
nextrws <rws@nextrade.co.kr>
2025-07-09 14:42:13 +0900

    미사용분리 테스트

--- net_admin/app/urls.py
+++ net_admin/app/urls.py
@@ -5,5 +5,5 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('multicast', views.multicast, name='multicast'),
+    # path('multicast', views.multicast, name='multicast'),
 ]
--- net_admin/app/views.py
+++ net_admin/app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index (request): 
     return render(request, 'index.html')
 
-def multicast (request): 
-    return render(request, 'multicast.html')
+# def multicast (request): 
+#     return render(request, 'multicast.html')
