3c2f28cfd1b700170bc720ab3faa036f6a1c1bf2
nextrws <rws@nextrade.co.kr>
2025-07-11 16:53:11 +0900

    장비접속 포맷변경 전으로 롤백

--- utils/netbox_api.py
+++ utils/netbox_api.py
@@ -4,7 +4,6 @@
 import requests
 import json
 import os
-import sys
 from dotenv import load_dotenv
 from typing import Dict, Any 
 from requests.auth import HTTPBasicAuth
@@ -16,11 +15,8 @@ NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
 NETBOX_URL = os.getenv("NETBOX_URL", "http://172.30.32.221:80")
 
 def main():
-    tag = sys.argv[1] if len(sys.argv) > 1 else ""
-    manufacturer = sys.argv[2] if len(sys.argv) > 2 else ""
-
     try:
-        device_info = get_netbox_device_info(tag=tag, manufacturer=manufacturer)    
+        device_info = get_netbox_device_info()
         print(f"Device Info: {device_info}")
     except Exception as e:
         print(f"Error fetching device info: {e}")
