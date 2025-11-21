e91767d67c136923212d4bfac9602748871ae572
nextrws <rws@nextrade.co.kr>
2025-11-21 11:08:55 +0900

    셋팅 설정

--- net_admin/multicast/apps.py
+++ net_admin/multicast/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MulticastConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app.multicast"
+    name = "multicast"
--- net_admin/net_admin/settings.py
+++ net_admin/net_admin/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'multicast',
-    'information',
+    'multicast.apps.MulticastConfig',
+    'information.apps.InformationConfig',
 ]
 
 MIDDLEWARE = [
